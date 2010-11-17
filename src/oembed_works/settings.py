775283d42e666eeecbd07b0787d61b3c498e9cbd
gnotaras <none@none>
2010-11-17 18:15:52 +0200

    Added OEMBED_PROVIDERS setting

--- src/oembed_works/settings.py
+++ src/oembed_works/settings.py
@@ -27,5 +27,35 @@
 from django.conf import settings
 
 
-MY_APP_SETTING = getattr(settings, 'MY_APP_SETTING', '...')
+
+_OEMBED_PROVIDERS = {
+    # Format:
+    #     <provider_name>: (<format>, <endpoint_api_url>, [<regex1>, <regex2>, ...])
+    
+    'Flickr': ('1', 'http://www.flickr.com/services/oembed',
+        [r'http://(?:www\.)?flickr\.com/photos/\S+?/(?:sets/)?\d+/?']),
+    'Amazon Product Image (OohEmbed)': ('1', 'http://oohembed.com/oohembed/',
+        [r'http://\S*\.amazon\.(com|co\.uk|de|ca|jp)/\S*/(gp/product|o/ASIN|obidos/ASIN|dp)/\S*']),
+    'Google Video (OohEmbed)': ('1', 'http://oohembed.com/oohembed/',
+        [r'http://video\.google\.com/videoplay?\S*']),
+    'Metacafe (OohEmbed)': ('1', 'http://oohembed.com/oohembed/',
+        [r'http://\S*.metacafe.com/watch/\S*']),
+    'Twitter Status (OohEmbed)': ('1', 'http://oohembed.com/oohembed/',
+        [r'http://(?:www\.)?twitter\.com/(?:\w{1,20})/statuses/\d+/?']),
+    'Wikipedia (OohEmbed)': ('1', 'http://oohembed.com/oohembed/',
+        [r'http://\S*.wikipedia.org/wiki/\S*']),
+    'YouTube (OohEmbed)': ('1', 'http://oohembed.com/oohembed/',
+        [r'http://(?:www\.)?youtube\.com/watch\?v=[A-Za-z0-9\-=_]{11}']),
+    'Vimeo': ('1', 'http://vimeo.com/api/oembed.json',
+        [r'http://(?:www\.)?vimeo\.com/\d+']),
+    'Scribd': ('1', 'http://www.scribd.com/services/oembed',
+        [r'http://(?:www\.)?scribd\.com/.*']),
+    'Dailymotion (OohEmbed)': ('1', 'http://oohembed.com/oohembed/',
+        [r'http://(?:www\.)?dailymotion\.com/video/[^<\S]+']),
+    #'': ('1', '',
+    #    [r'']),
+}
+
+OEMBED_PROVIDERS = getattr(settings, 'OEMBED_PROVIDERS', _OEMBED_PROVIDERS)
+
 
